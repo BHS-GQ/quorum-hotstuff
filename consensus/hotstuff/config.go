20da0940a3e7b526255830b9835eae7eb1b45454
Derick Wells <wowells@up.edu.ph>
2022-12-27 12:32:16 +0800

    Add some hotstuff package interfaces/structs

--- consensus/hotstuff/config.go
+++ consensus/hotstuff/config.go
@@ -1,19 +1,3 @@
-// Copyright 2017 The go-ethereum Authors
-// This file is part of the go-ethereum library.
-//
-// The go-ethereum library is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// The go-ethereum library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
-
 package hotstuff
 
 type SelectProposerPolicy uint64
@@ -28,11 +12,11 @@ type Config struct {
 	RequestTimeout uint64               `toml:",omitempty"` // The timeout for each Istanbul round in milliseconds.
 	BlockPeriod    uint64               `toml:",omitempty"` // Default minimum difference between two consecutive block's timestamps in second for basic hotstuff and mill-seconds for event-driven
 	LeaderPolicy   SelectProposerPolicy `toml:",omitempty"` // The policy for speaker selection
-	Test           bool                 `toml:",omitempty"`
+	Test           bool                 `toml:",omitempty"` // Flag for unit tests
 	Epoch          uint64               `toml:",omitempty"` // The number of blocks after which to checkpoint and reset the pending votes
 }
 
-// todo: modify request timeout, and miner recommit default value is 3s. recommit time should be > blockPeriod
+// [TODO] Modify RequestTimeout; recommit time should be > blockPeriod
 var DefaultBasicConfig = &Config{
 	RequestTimeout: 4000,
 	BlockPeriod:    1,
--- consensus/hotstuff/signer.go
+++ consensus/hotstuff/signer.go
@@ -9,16 +9,40 @@ type Signer interface {
 	Address() common.Address
 
 	// BLS
+
+	// BLSSign signs data with partial private key of validator
 	BLSSign(data []byte) ([]byte, error)
+
+	// BLSRecoverAggSig recovers aggregated signature for data
+	// given partially-signed signatures in sigShares. Intended
+	// for HotStuff leaders
 	BLSRecoverAggSig(data []byte, sigShares [][]byte) ([]byte, error)
+
+	// BLSVerifyAggSig verifies aggregated signature over data.
+	// Intended for HotStuff replicas
 	BLSVerifyAggSig(data []byte, aggSig []byte) error
 
-	// Not BLS
-	Sign(data []byte) ([]byte, error)
-	SealHash(header *types.Header) (hash common.Hash)
-	Recover(header *types.Header) (common.Address, error)
-	SealBeforeCommit(h *types.Header) error
-	CheckSignature(valSet ValidatorSet, data []byte, sig []byte) (common.Address, error)
-	PrepareExtra(header *types.Header, valSet ValidatorSet) ([]byte, error)
+	// VerifyQC verifies a QC code, view, and hash given the aggsig
+	// Intended for HotStuff replicas
 	VerifyQC(qc *QuorumCert) error
+
+	// Not BLS
+
+	// Sign signs data for ECDSA authetication
+	Sign(data []byte) ([]byte, error)
+
+	// SealHash returns block hash before sealing
+	HeaderHash(header *types.Header) (hash common.Hash)
+
+	// Recover extracts the proposer address from a signed header
+	HeaderRecoverProposer(header *types.Header) (common.Address, error)
+
+	// SignerSeal proposer signs header with ECDSA
+	SignerSeal(h *types.Header) error
+
+	// CheckSignature checks if ECDSA-signed data comes from a validator
+	CheckSignature(valSet ValidatorSet, data []byte, sig []byte) (common.Address, error)
+
+	// BuildPrepareExtra builds a block's Extra field for Prepare stage
+	BuildPrepareExtra(header *types.Header, valSet ValidatorSet) ([]byte, error)
 }
