fe296a3a1196b3af10c2d43df03f2c0ed9866681
Derick Wells <wowells@up.edu.ph>
2023-01-31 16:05:45 +0800

    Add more prepare vote tests

--- consensus/hotstuff/mock/mock_prepare_test.go
+++ consensus/hotstuff/mock/mock_prepare_test.go
@@ -593,3 +593,225 @@ func TestMockPrepareVoteCase3(t *testing.T) {
 		t.Fail()
 	}
 }
+
+// go test -v -count=1 github.com/ethereum/go-ethereum/consensus/hotstuff/mock -run TestMockPrepareVoteCase1
+// net scale is 4, leader is sent fake message of prepareVote with wrong height.
+func TestMockPrepareVoteCase4(t *testing.T) {
+	H, R, fH, fN := uint64(4), uint64(0), uint64(5), 2
+	fakeNodes := make(map[common.Address]struct{})
+	mu := new(sync.Mutex)
+
+	sys := makeSystem(4)
+	sys.Start()
+	time.Sleep(2 * time.Second)
+
+	hasViewChange := false
+	hook := func(node *Geth, data []byte) ([]byte, bool) {
+		h, r := node.api.CurrentSequence()
+		if h == H && r == R {
+			if node.IsProposer() {
+				return data, true
+			}
+
+			mu.Lock()
+			if _, ok := fakeNodes[node.addr]; ok {
+				mu.Unlock()
+				return data, true
+			}
+			mu.Unlock()
+
+			var ori hs.Message
+			if err := rlp.DecodeBytes(data, &ori); err != nil {
+				log.Error("failed to decode message", "err", err)
+				return data, true
+			}
+			if ori.Code != hs.MsgTypePrepareVote {
+				return data, true
+			}
+			msg := ori.Copy()
+			msg.View.Height = new(big.Int).SetUint64(fH)
+
+			payload, err := node.resignMsg(msg)
+			if err != nil {
+				log.Error("failed to resign message", "err", err)
+				return data, true
+			}
+
+			mu.Lock()
+			fakeNodes[node.addr] = struct{}{}
+			if len(fakeNodes) > fN {
+				mu.Unlock()
+				return data, true
+			}
+			mu.Unlock()
+
+			view := &hs.View{
+				Round:  new(big.Int).SetUint64(r),
+				Height: new(big.Int).SetUint64(h),
+			}
+			log.Info("-----fake message", "address", node.addr, "msg", msg.Code, "view", view, "msg", msg)
+			return payload, true
+		}
+		if h == H && r == R+1 {
+			hasViewChange = true
+		}
+		return data, true
+	}
+
+	for _, node := range sys.nodes {
+		node.setHook(hook)
+	}
+	sys.Close(10)
+
+	if !hasViewChange {
+		t.Fail()
+	}
+}
+
+// go test -v -count=1 github.com/ethereum/go-ethereum/consensus/hotstuff/mock -run TestMockPrepareVoteCase2
+// net scale is 4, leader send fake message of prepareVote with wrong round.
+func TestMockPrepareVoteCase5(t *testing.T) {
+	H, R, fR, fN := uint64(4), uint64(0), uint64(1), 2
+	fakeNodes := make(map[common.Address]struct{})
+	mu := new(sync.Mutex)
+
+	sys := makeSystem(4)
+	sys.Start()
+	time.Sleep(2 * time.Second)
+
+	hasViewChange := false
+	hook := func(node *Geth, data []byte) ([]byte, bool) {
+		h, r := node.api.CurrentSequence()
+		if h == H && r == R {
+			if node.IsProposer() {
+				return data, true
+			}
+
+			mu.Lock()
+			if _, ok := fakeNodes[node.addr]; ok {
+				mu.Unlock()
+				return data, true
+			}
+			mu.Unlock()
+
+			var ori hs.Message
+			if err := rlp.DecodeBytes(data, &ori); err != nil {
+				log.Error("failed to decode message", "err", err)
+				return data, true
+			}
+			if ori.Code != hs.MsgTypePrepareVote {
+				return data, true
+			}
+			msg := ori.Copy()
+			msg.View.Round = new(big.Int).SetUint64(fR)
+
+			payload, err := node.resignMsg(msg)
+			if err != nil {
+				log.Error("failed to resign message", "err", err)
+				return data, true
+			}
+
+			mu.Lock()
+			fakeNodes[node.addr] = struct{}{}
+			if len(fakeNodes) > fN {
+				mu.Unlock()
+				return data, true
+			}
+			mu.Unlock()
+
+			view := &hs.View{
+				Round:  new(big.Int).SetUint64(r),
+				Height: new(big.Int).SetUint64(h),
+			}
+			log.Info("-----fake message", "address", node.addr, "msg", msg.Code, "view", view, "msg", msg)
+			return payload, true
+		}
+		if h == H && r == R+1 {
+			hasViewChange = true
+		}
+		return data, true
+	}
+
+	for _, node := range sys.nodes {
+		node.setHook(hook)
+	}
+	sys.Close(10)
+
+	if !hasViewChange {
+		t.Fail()
+	}
+}
+
+// go test -v -count=1 github.com/ethereum/go-ethereum/consensus/hotstuff/mock -run TestMockPrepareVoteCase3
+// net scale is 4, leader send fake message of prepareVote with wrong digest.
+func TestMockPrepareVoteCase6(t *testing.T) {
+	H, R, fN := uint64(4), uint64(0), 2
+	fakeNodes := make(map[common.Address]struct{})
+	mu := new(sync.Mutex)
+
+	sys := makeSystem(4)
+	sys.Start()
+	time.Sleep(2 * time.Second)
+
+	hasViewChange := false
+	hook := func(node *Geth, data []byte) ([]byte, bool) {
+		h, r := node.api.CurrentSequence()
+		if h == H && r == R {
+			if node.IsProposer() {
+				return data, true
+			}
+
+			mu.Lock()
+			if _, ok := fakeNodes[node.addr]; ok {
+				mu.Unlock()
+				return data, true
+			}
+			mu.Unlock()
+
+			var ori hs.Message
+			if err := rlp.DecodeBytes(data, &ori); err != nil {
+				log.Error("failed to decode message", "err", err)
+				return data, true
+			}
+			if ori.Code != hs.MsgTypePrepareVote {
+				return data, true
+			}
+			msg := ori.Copy()
+			msg.Msg = common.HexToHash("0x12345").Bytes()
+
+			payload, err := node.resignMsg(msg)
+			if err != nil {
+				log.Error("failed to resign message", "err", err)
+				return data, true
+			}
+
+			mu.Lock()
+			fakeNodes[node.addr] = struct{}{}
+			if len(fakeNodes) > fN {
+				mu.Unlock()
+				return data, true
+			}
+			mu.Unlock()
+
+			view := &hs.View{
+				Round:  new(big.Int).SetUint64(r),
+				Height: new(big.Int).SetUint64(h),
+			}
+			log.Info("-----fake message", "address", node.addr, "msg", msg.Code, "view", view, "msg", msg)
+			return payload, true
+		}
+		if h == H && r == R+1 {
+			hasViewChange = true
+		}
+		return data, true
+	}
+
+	for _, node := range sys.nodes {
+		node.setHook(hook)
+	}
+	sys.Close(10)
+
+	if !hasViewChange {
+		t.Fail()
+	}
+}
