46788adf2b321f8c5b5b9eff42a201b6296da568
Derick Wells <wowells@up.edu.ph>
2023-02-08 17:16:25 +0800

    Add prepare test when faulty QC is sent to =<F nodes

--- consensus/hotstuff/mock/mock_prepare_test.go
+++ consensus/hotstuff/mock/mock_prepare_test.go
@@ -3,6 +3,7 @@ package mock
 import (
 	"math/big"
 	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -355,6 +356,99 @@ func TestMockPrepareCase5(t *testing.T) {
 			}
 			log.Info("-----fake message", "address", node.addr, "msg", msg.Code, "view", view, "msg", msg)
 			return payload, true
+		}
+		if h == H && r == R+1 {
+			hasViewChange = true
+		}
+		return data, true
+	}
+
+	for _, node := range sys.nodes {
+		node.setHook(hook)
+	}
+	sys.Close(10)
+
+	if !hasViewChange {
+		t.Fail()
+	}
+}
+
+// go test -v -count=1 github.com/ethereum/go-ethereum/consensus/hotstuff/mock -run TestMockPrepareCase6
+// net scale is 4, leader send fake message of prepare with gibberish BLSSignature, repos WONT change view.
+func TestMockPrepareCase6(t *testing.T) {
+	H, R, fN := uint64(4), uint64(0), int32(1)
+
+	var locked int32
+	atomic.StoreInt32(&locked, 0)
+
+	sys := makeSystem(4)
+	sys.Start()
+	time.Sleep(2 * time.Second)
+
+	hasViewChange := false
+	hook := func(node *Geth, data []byte) ([]byte, bool) {
+		h, r := node.api.CurrentSequence()
+		if h == H && r == R {
+			if !node.IsProposer() {
+				return data, true
+			}
+
+			var ori hs.Message
+			if err := rlp.DecodeBytes(data, &ori); err != nil {
+				log.Error("failed to decode message", "err", err)
+				return data, true
+			}
+			if ori.Code != hs.MsgTypePrepare {
+				return data, true
+			}
+			msg := ori.Copy()
+			var sub hs.PackagedQC
+			if err := rlp.DecodeBytes(msg.Msg, &sub); err != nil {
+				log.Error("failed to decode subject", "err", err)
+				return data, true
+			}
+			var qc hs.QuorumCert
+			if raw, err := rlp.EncodeToBytes(sub.QC); err != nil {
+				log.Error("failed to encode prepareQC", "err", err)
+				return data, true
+			} else if err := rlp.DecodeBytes(raw, &qc); err != nil {
+				log.Error("failed to decode prepareQC", "err", err)
+				return data, true
+			}
+
+			qc.BLSSignature[0] += 1
+
+			var newSub = struct {
+				Node *hs.TreeNode
+				QC   *hs.QuorumCert
+			}{
+				sub.TreeNode,
+				&qc,
+			}
+			if raw, err := rlp.EncodeToBytes(newSub); err != nil {
+				log.Error("failed to encode new subject", "err", err)
+				return data, true
+			} else {
+				msg.Msg = raw
+			}
+
+			payload, err := node.resignMsg(msg)
+			if err != nil {
+				log.Error("failed to resign message")
+				return data, true
+			}
+
+			if value := atomic.LoadInt32(&locked); value >= fN {
+				return data, true
+			} else {
+				atomic.StoreInt32(&locked, value+1)
+				view := &hs.View{
+					Round:  new(big.Int).SetUint64(r),
+					Height: new(big.Int).SetUint64(h),
+				}
+				log.Info("-----fake message", "address", node.addr, "msg", msg.Code, "view", view, "msg", msg)
+				return payload, true
+			}
 		}
 		if h == H && r == R+1 {
 			hasViewChange = true
